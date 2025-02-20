ee1f163de8a4e5b26336b58739e6f62b40082c7b
Cuong Dang <ngoccuongth02@gmail.com>
2025-02-21 00:37:59 +0700

    feat: update logic to handle update

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,13 +2,18 @@ import { createNote, deleteNote, getNotes, openExternal, readNote, writeNote } f
 import { electronApp, is, optimizer } from '@electron-toolkit/utils';
 import { CreateNote, DeleteNote, GetNotes, OpenExternal, ReadNote, WriteNote } from '@shared/types';
 import { BrowserWindow, app, ipcMain, shell } from 'electron';
+import { autoUpdater } from 'electron-updater';
 import { join } from 'path';
 import icon from '../../resources/icon.icns?asset';
+
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+let mainWindow: BrowserWindow;
 
 function createWindow(): void {
     // Create the browser window.
 
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 1920,
         height: 1080,
         show: false,
@@ -76,6 +81,27 @@ app.whenReady().then(() => {
         // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
+
+    autoUpdater.checkForUpdates();
+});
+
+/*New Update Available*/
+autoUpdater.on("update-available", () => {
+  console.log('update-available');
+  autoUpdater.downloadUpdate();
+});
+
+autoUpdater.on("update-not-available", () => {
+  console.log(`No update available. Current version ${app.getVersion()}`);
+});
+
+/*Download Completion Message*/
+autoUpdater.on("update-downloaded", () => {
+  console.log(`Update downloaded. Current version ${app.getVersion()}`);
+});
+
+autoUpdater.on("error", (info) => {
+  console.log(info);
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
