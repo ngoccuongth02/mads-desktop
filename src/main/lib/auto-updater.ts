1db29552ecd3af143ce12e320d6dae80c56ac4b0
Cuong Dang <ngoccuongth02@gmail.com>
2025-02-22 00:47:18 +0700

    feat: add auto updater

--- src/main/lib/auto-updater.ts
+++ src/main/lib/auto-updater.ts
@@ -1,9 +1,8 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, dialog } from 'electron';
 import { autoUpdater, ProgressInfo, UpdateDownloadedEvent } from 'electron-updater';
 
 export const init = (mainWindow: BrowserWindow): void => {
   autoUpdater.autoDownload = false;
-  autoUpdater.autoInstallOnAppQuit = true;
   initEvent(mainWindow);
   autoUpdater.checkForUpdates();
 }
@@ -12,6 +11,11 @@ const initEvent = (mainWindow: BrowserWindow) => {
   autoUpdater.on('update-available', () => {
     mainWindow?.webContents.send('updateMessage', 'update-available');
     autoUpdater.downloadUpdate();
+    dialog.showMessageBox({
+      type: "info",
+      title: "Update Available",
+      message: "A new version is available. Downloading now...",
+    });
   });
 
   autoUpdater.on('update-not-available', () => {
@@ -20,12 +24,18 @@ const initEvent = (mainWindow: BrowserWindow) => {
 
   autoUpdater.on('download-progress', (progress: ProgressInfo) => {
     mainWindow?.webContents.send('updateMessage', `
-      Updating: ${progress.transferred}/${progress.total} - ${progress.bytesPerSecond}/s - ${progress.percent} - ${progress.delta}
+      Downloaded: ${progress.transferred}/${progress.total} - Speed: ${progress.bytesPerSecond}/s - % done: ${progress.percent}
     `);
   });
 
   autoUpdater.on('update-downloaded', (event: UpdateDownloadedEvent) => {
-    mainWindow?.webContents.send('updateMessage', `Update downloaded. Current version ${app.getVersion()} ${event}`);
+    mainWindow?.webContents.send('updateMessage', `Update downloaded. New version ${event.version}`);dialog.showMessageBox({
+      type: "info",
+      title: "Update Ready",
+      message: "Update downloaded. Application will now restart.",
+    }).then(() => {
+      autoUpdater.quitAndInstall();
+    });
   });
 
   autoUpdater.on('error', (info) => {
