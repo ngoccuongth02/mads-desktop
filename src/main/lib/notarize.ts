59dc1890d00f7a8ee7196d58bf5c2b0d3c9013d6
Cuong Dang <ngoccuongth02@gmail.com>
2025-03-05 22:35:50 +0700

    build: update notarize

--- src/main/lib/notarize.ts
+++ src/main/lib/notarize.ts
@@ -19,11 +19,16 @@ export default async function notarizing(context: BuildResult) {
   console.log("🔑 Notarizing the app...");
   try {
     await notarize({
-      appBundleId,
+      // appBundleId,
+      // appPath: `${appOutDir}/${appName}.app`,
+      // appleId: process.env.APPLE_ID || "",
+      // appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD || "",
+      // teamId: process.env.APPLE_TEAM_ID || "",
+      tool: "notarytool", // Ensure we use the modern notarization method
       appPath: `${appOutDir}/${appName}.app`,
+      teamId: process.env.APPLE_TEAM_ID || "", // Required for notarytool
       appleId: process.env.APPLE_ID || "",
       appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD || "",
-      teamId: process.env.APPLE_TEAM_ID || "",
     });
     console.log("✅ Notarization complete.");
   } catch (error) {
